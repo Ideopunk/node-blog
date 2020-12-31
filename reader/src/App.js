afc8f925303c6c54824eaba3e31bd1436f7828c8
Conor Barnes <conorbarnes93@gmail.com>
2020-12-31 09:27:52 -0500

    initialize reader site

---
+++ reader/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8080";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
